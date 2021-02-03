093043329a5ea01d3b0d69c58a8d1d82a0a3a5f9
Lucas Ferguson <lucasferguson16@gmail.com>
2021-02-02 19:55:59 -0600

    2/2/2021 SwerveModule

--- src/main/java/frc/robot/subsystems/DriveTrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/DriveTrain/SwerveModule.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.DriveTrain;
 
 import com.revrobotics.* ;
 import com.ctre.phoenix.sensors.*;
+import edu.wpi.first.wpilibj.controller.PIDController;
 
 public class SwerveModule {
 
@@ -13,6 +14,7 @@ public class SwerveModule {
 
     private CANCoder m_steerEncoder ;
 
+    private PIDController m_pidController ;
 
     public SwerveModule(int driveMotorID, int steerMotorID, int steerEncoderId ){
         
@@ -24,6 +26,41 @@ public class SwerveModule {
         m_driveMotorEncoder = m_driveMotor.getEncoder() ;
         m_steerMotorEncoder = m_steerMotor.getEncoder() ;
 
+        m_pidController = new PIDController(20, 10, 0);
+
+        
+        CANPIDController m_SparkMaxpidController = m_steerMotor.getPIDController();
+
+        double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+
+        // PID coefficients
+        kP = 6e-5;
+        kI = 6e-7;
+        kD = 0;
+        kIz = 0;
+        kFF = 0.000015;
+        kMaxOutput = 1;
+        kMinOutput = -1;
+        maxRPM = 5700 / 2;
+
+        // set PID coefficients
+        m_SparkMaxpidController.setP(kP);
+        m_SparkMaxpidController.setI(kI);
+        m_SparkMaxpidController.setD(kD);
+        m_SparkMaxpidController.setIZone(kIz);
+        m_SparkMaxpidController.setFF(kFF);
+        m_SparkMaxpidController.setOutputRange(kMinOutput, kMaxOutput);
+
     }
+
+
+
+        double setVel = m_pidController.calculate(m_steerEncoder.getPosition(), angle_IN);
+
+        // double setPoint = m_stick.getY() * maxRPM;
+        m_pidController.setReference(setVel, ControlType.kVelocity);
+        
+
+
     
 }
