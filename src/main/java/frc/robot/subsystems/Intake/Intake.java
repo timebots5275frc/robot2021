44631ab675c30c58d51af56b261a2e3decca44bc
Allen <stonemail43@gmail.com>
2021-02-08 19:34:21 -0600

    2/8/21 intake Pnuematics

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,10 +1,30 @@
 package frc.robot.subsystems.Intake;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Solenoid;
 
 /**
  * Description:
  */
 public class Intake extends SubsystemBase {
-    
+    Solenoid solenoid1 = new Solenoid(1);
+    // IMPORTANT
+    int Bob = 1;
+
+    // Setting solenoid to true
+    public void setSolenoidTrue() {
+        solenoid1.set(true);
+    }
+
+    // Setting solenoid to false
+    public void setSolenoidFalse() {
+        solenoid1.set(false);
+    }
+
+    // Class toggleSolinoid will toggle it
+    public void toggleSolinoid() {
+        solenoid1.toggle();
+
+    }
 }
