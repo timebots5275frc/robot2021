804ad9e632367f7d8d65d7cfd3637d71a5294e34
Caid-Peterson-Time-bots <78391762+Caid-Peterson-Time-bots@users.noreply.github.com>
2021-02-11 19:09:14 -0600

    2/11/21 HopperDefault

--- src/main/java/frc/robot/subsystems/Hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper/Hopper.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Hopper;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Constants;
@@ -11,5 +13,8 @@ public class Hopper extends SubsystemBase {
     TalonSRX motor1 = new TalonSRX(Constants.HopperConstants.HOPPER_MOTOR_ID);
     public Hopper (){
 
+    }
+    public void setMototrSpeed(double input){
+motor1.set(ControlMode.PercentOutput, input);
     }
 }
