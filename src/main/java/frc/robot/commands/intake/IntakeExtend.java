35b79fb5e1c5109acb55bdb7c2f86085365c9fb0
Allen <stonemail43@gmail.com>
2021-02-13 11:28:13 -0600

    2/13/2021 Compressor On and Off

---
+++ src/main/java/frc/robot/commands/intake/IntakeExtend.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+
+public class IntakeExtend extends CommandBase {
+  private Intake subsystem;
+
+  public IntakeExtend(Intake subsystem) {
+    this.subsystem = subsystem;
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    this.subsystem.setSolenoidTrue();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
