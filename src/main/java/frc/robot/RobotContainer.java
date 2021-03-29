8e56091c641b3221de9fcdf10bde787898cd5206
Lucas <lucasferguson16@gmail.com>
2021-03-29 13:14:14 -0500

    // remove comments //

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import frc.robot.constants.Constants;
 import frc.robot.constants.Constants.AutoConstants;
-import frc.robot.constants.Constants.DriveConstants;
 import frc.robot.subsystems.driveTrain.DriveHome;
 import frc.robot.subsystems.driveTrain.DriveTrain;
 import frc.robot.subsystems.driveTrain.JoystickDrive;
@@ -17,26 +16,15 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
-// import edu.wpi.first.wpilibj.examples.swervecontrollercommand.Constants.AutoConstants;
-// import edu.wpi.first.wpilibj.examples.swervecontrollercommand.Constants.DriveConstants;
-// import edu.wpi.first.wpilibj.examples.swervecontrollercommand.Constants.OIConstants;
-// import edu.wpi.first.wpilibj.examples.swervecontrollercommand.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
